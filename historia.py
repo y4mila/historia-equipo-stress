71866b64feb7d5545a9bec1ba7e59d4b4a5adbeb
JS121xx <josuesalazarxx@gmail.com>
2025-08-04 17:47:13 -0600

    Parte 2 de la historia

--- historia.py
+++ historia.py
@@ -1,2 +1,4 @@
 #integrante 1
 print("Parte 1: Ana vivía en un pueblo tranquilo. Un día, encontró una piedra brillante en el bosque y todo empezó a cambiar.")
+#integrante 2
+print("Parte 2: Ana habia encontrado una piedra magica que la hacia volar. ")
