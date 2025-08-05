0ad01813ee70bc70a59551519351e685fe8d7015
y4mla <2024310240@uteq.edu.mx>
2025-08-04 18:00:56 -0600

    Ups, borré todo sin querer

--- historia.py
+++
@@ -1,6 +0,0 @@
-#integrante 1
-print("Parte 1: Ana vivía en un pueblo tranquilo. Un día, encontró una piedra brillante en el bosque y todo empezó a cambiar.")
-#integrante 2
-print("Parte 2: Ana habia encontrado una piedra magica que la hacia volar. ")
-#integrante 3
-print("Parte 3: Con su nuevo poder, Ana decidió explorar el mundo y ayudar a quienes lo necesitaban.")
